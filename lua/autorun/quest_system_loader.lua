dd23b853c7733ed856fa87edfe57785288e9da2c
Shark-vil <shark_vil@mail.ru>
2020-12-03 19:17:34 +0300

    Added automatic script search

--- lua/autorun/quest_system_loader.lua
+++ lua/autorun/quest_system_loader.lua
@@ -1,8 +1,17 @@
 local root_directory = 'quest_system'
 
-local function using(local_file_path, network_type)
-    local file_path = root_directory .. '/' .. local_file_path .. '.lua'
+local function getFullPathToFile(local_file_path, not_root_directory)
+    if not not_root_directory then
+        return root_directory .. '/' .. local_file_path
+    else
+        return local_file_path
+    end
+end
+
+local function using(local_file_path, network_type, not_root_directory)
+    local file_path = getFullPathToFile(local_file_path, not_root_directory)    
     network_type = network_type or 'sh'
+    network_type = string.lower(network_type)
 
     if network_type == 'cl' or network_type == 'sh' then
         if SERVER then AddCSLuaFile(file_path) end
@@ -12,5 +21,19 @@ local function using(local_file_path, network_type)
     end
 end
 
-using('main')
-using('tools/trigger_tool')
+local function auto_using(local_directory_path, not_root_directory)
+    local files, directories = file.Find(local_directory_path .. "/*", "LUA")
+    
+    for _, file_path in pairs(files) do
+        local type = string.sub(file_path, 1, 2)
+        using(local_directory_path .. '/' .. file_path, type, true)
+    end
+
+    for _, directory_path in pairs(directories) do
+        auto_using(local_directory_path .. '/' .. directory_path, true)
+    end
+end
+
+auto_using(root_directory)
+
+getFullPathToFile, using, auto_using, root_directory = nil
