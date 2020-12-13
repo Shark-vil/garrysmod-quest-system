7efd3e94dd16c897b8921e1a74e5a113ae91de87
Shark-vil <shark_vil@mail.ru>
2020-12-13 21:00:10 +0300

    Improved quest board design

--- lua/quest_system/gui/simple_quest_menu/cl_menu.lua
+++ lua/quest_system/gui/simple_quest_menu/cl_menu.lua
@@ -1,9 +1,12 @@
 local function OpenMenu()
     local Frame = vgui.Create('DFrame')
-    Frame:SetTitle('Simple quest menu')
+    Frame:SetTitle('Доска заданий')
     Frame:SetSize(500, 450)
     Frame:MakePopup()
     Frame:Center()
+    Frame.Paint = function(self, w, h )
+        draw.RoundedBox(8, 0, 0, w, h, Color(0, 0, 0))
+    end
 
     local ScrollPanel = vgui.Create("DScrollPanel", Frame)
     ScrollPanel:Dock(FILL)
@@ -16,9 +19,12 @@ local function OpenMenu()
             PanelItem:SetHeight(120)
             PanelItem:Dock(TOP)
             PanelItem:DockMargin(0, 0, 0, 5)
+            PanelItem.Paint = function(self, w, h )
+                draw.RoundedBox(4, 0, 0, w, h, Color(220, 225, 220, 200))
+            end
 
             local LabelTitle = vgui.Create("DLabel", PanelItem)
-            LabelTitle:SetPos(1, 1)
+            LabelTitle:SetPos(5, 5)
             LabelTitle:SetFont('SimpleQuestMenuTitle')
             LabelTitle:SetText(quest.title)
             LabelTitle:SizeToContents()
@@ -30,6 +36,9 @@ local function OpenMenu()
             PanelDescription.Paint = function(self, w, h)
                 surface.SetDrawColor(100, 100, 100, 50)
                 surface.DrawRect(0, 0, w, h)
+            end
+            PanelDescription.Paint = function(self, w, h )
+                draw.RoundedBox(0, 0, 0, w, h, Color(220, 225, 220, 255))
             end
 
             local LabelDescription = vgui.Create("DLabel", PanelDescription)
@@ -44,7 +53,7 @@ local function OpenMenu()
 
             if LocalPlayer():QuestIsActive(id) then
                 local ButtonQuestDisable = vgui.Create('DButton', PanelItem)
-                ButtonQuestDisable:SetText('Отменить квест')
+                ButtonQuestDisable:SetText('Отозвать задание')
                 ButtonQuestDisable:Dock(BOTTOM)
                 ButtonQuestDisable.DoClick = function()
                     net.Start('sv_qsystem_stopquest')
@@ -54,7 +63,7 @@ local function OpenMenu()
                 end
             else
                 local ButtonQuestEnable = vgui.Create('DButton', PanelItem)
-                ButtonQuestEnable:SetText('Начать квест')
+                ButtonQuestEnable:SetText('Взять задание')
                 ButtonQuestEnable:Dock(BOTTOM)
                 ButtonQuestEnable.DoClick = function()
                     net.Start('sv_qsystem_startquest')
