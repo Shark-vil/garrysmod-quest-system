27be38676f5fd1fdefb4132283e6831c41b1fbcb
Shark-vil <shark_vil@mail.ru>
2020-12-18 05:36:27 +0300

    Improved smooth deletion

--- lua/quest_system/extension/sv_entity_remove.lua
+++ lua/quest_system/extension/sv_entity_remove.lua
@@ -8,14 +8,57 @@ function meta:FadeRemove(delay, minus)
 
     self:SetRenderMode(RENDERMODE_TRANSCOLOR)
     self:SetCollisionGroup(COLLISION_GROUP_WORLD)
+
+    local weapon = NULL
+    local IsNPC = self:IsNPC()
+    if IsNPC then
+        self:SetSchedule(SCHED_IDLE_STAND)
+        self:CapabilitiesClear()
+        weapon = self:GetActiveWeapon()
+        if IsValid(weapon) then
+            weapon:SetRenderMode(RENDERMODE_TRANSCOLOR)
+        end
+    end
     self.Use = function() end
+
+    hook.Add('EntityTakeDamage', self, function(this, ent)
+        if IsValid(self) and self == ent then
+            return true
+        end
+    end)
+
+    hook.Add("PhysgunPickup", self, function(this, ply, ent)
+        if IsValid(self) and self == ent then
+            return false
+        end
+    end)
     
     hook.Add('Think', self, function()
         if IsValid(self) then
+            if IsNPC then
+                local target = self:GetTarget()
+                if IsValid(target) then
+                    self:AddEntityRelationship(target, D_NU, 99)
+                end
+                local enemy = self:GetEnemy()
+                if IsValid(enemy) then
+                    self:AddEntityRelationship(enemy, D_NU, 99)
+                end
+                self:ClearGoal()
+                self:ClearSchedule()
+            end
+
             if delay < CurTime() then
                 local color = self:GetColor()
                 if color.a - minus >= 0 then
-                    self:SetColor(ColorAlpha(color, color.a - minus))
+                    local newColor = ColorAlpha(color, color.a - minus)
+                    self:SetColor(newColor)
+                    if IsNPC then
+                        local weapon = self:GetActiveWeapon()
+                        if IsValid(weapon) then
+                            weapon:SetColor(newColor)
+                        end
+                    end
                 else
                     self:Remove()
                 end
